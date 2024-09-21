c89bc8afe17444dcdfeb8ca160f3b38c5968e0bc
CHARITH127 <charithrullz993@gmail.com>
2024-09-21 12:31:30 +0530

    Add droped file

---
+++ src/main/java/com/app/TaxiReservation/dto/ReservationDetailsDto.java
@@ -0,0 +1,23 @@
+package com.app.TaxiReservation.dto;
+
+import com.app.TaxiReservation.util.Status.ReservationStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReservationDetailsDto {
+    private Integer id;
+    private UserDto userDto;
+    private LocalDateTime reveredTime;
+    private double paymentAmount;
+    private double pickupLatitude;
+    private double pickupLongitude;
+    private double dropLatitude;
+    private double dropLongitude;
+    private ReservationStatus status;
+}
